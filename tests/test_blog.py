876cd267e9abd39a159f9272b906b88bc0db4e01
Wayne Chen <ntnumath.ematic@gmail.com>
2020-07-21 18:42:47 +0800

    tests script finished, remain testing

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -44,3 +44,38 @@ def test_author_required(app, client, auth):
 def test_exists_required(client, auth, path):
     auth.login()
     assert client.post(path.status_code == 404
+
+def test_create(client, auth, app):
+    auth.login()
+    assert client.get('/create').status_code == 200
+    client.post('/create', data={'title': 'created', 'body': ''})
+    
+    with app.app_context():
+        db = get_db()
+        count = db.execute('SELCT COUNT(id) FROM post ').fetchone()[0]
+        assert count == 2
+        
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get('/1/update').status_code == 200
+    client.post('/1/update', data={'title': 'updated', 'body': ''})
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post['title'] == 'updated'
+
+@pytest.mark.parametrize('path', (
+    'created',
+    '/1/update',
+))
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post('/1/delete')
+    assert response.headers['Location'] == 'http://localhost/'
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post is None
+
